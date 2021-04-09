e6cdf23c156fcf01005d788bcfb25e003445370f
Faayakhun <faayakhun@gmail.com>
2021-04-09 08:56:54 +0700

    first push

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import {BrowserRouter as Router} from "react-router-dom"
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Final Project Front End</h1>
+      <Router>
+
+      </Router>
+    </div>
+  );
+}
+
+export default App;
